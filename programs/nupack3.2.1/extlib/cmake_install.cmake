ec56aee2925d29fae5ce759b2864b50df33a818c
igemmunich2017 <33215074+igemmunich2017@users.noreply.github.com>
2017-10-30 07:58:16 +0100

    Add files via upload

---
+++ programs/nupack3.2.1/extlib/cmake_install.cmake
@@ -0,0 +1,133 @@
+# Install script for directory: /Users/sven/Desktop/IGEM/Program/progress/nupack/nupack3.2.1/extlib
+
+# Set the install prefix
+if(NOT DEFINED CMAKE_INSTALL_PREFIX)
+  set(CMAKE_INSTALL_PREFIX "/usr/local")
+endif()
+string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
+
+# Set the install configuration name.
+if(NOT DEFINED CMAKE_INSTALL_CONFIG_NAME)
+  if(BUILD_TYPE)
+    string(REGEX REPLACE "^[^A-Za-z0-9_]+" ""
+           CMAKE_INSTALL_CONFIG_NAME "${BUILD_TYPE}")
+  else()
+    set(CMAKE_INSTALL_CONFIG_NAME "Release")
+  endif()
+  message(STATUS "Install configuration: \"${CMAKE_INSTALL_CONFIG_NAME}\"")
+endif()
+
+# Set the component getting installed.
+if(NOT CMAKE_INSTALL_COMPONENT)
+  if(COMPONENT)
+    message(STATUS "Install component: \"${COMPONENT}\"")
+    set(CMAKE_INSTALL_COMPONENT "${COMPONENT}")
+  else()
+    set(CMAKE_INSTALL_COMPONENT)
+  endif()
+endif()
+
+if("${CMAKE_INSTALL_COMPONENT}" STREQUAL "Unspecified" OR NOT CMAKE_INSTALL_COMPONENT)
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/include/nupack/../extlib/tclap" TYPE FILE FILES "/Users/sven/Desktop/IGEM/Program/progress/nupack/nupack3.2.1/extlib/tclap/Arg.h")
+endif()
+
+if("${CMAKE_INSTALL_COMPONENT}" STREQUAL "Unspecified" OR NOT CMAKE_INSTALL_COMPONENT)
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/include/nupack/../extlib/tclap" TYPE FILE FILES "/Users/sven/Desktop/IGEM/Program/progress/nupack/nupack3.2.1/extlib/tclap/ArgException.h")
+endif()
+
+if("${CMAKE_INSTALL_COMPONENT}" STREQUAL "Unspecified" OR NOT CMAKE_INSTALL_COMPONENT)
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/include/nupack/../extlib/tclap" TYPE FILE FILES "/Users/sven/Desktop/IGEM/Program/progress/nupack/nupack3.2.1/extlib/tclap/ArgTraits.h")
+endif()
+
+if("${CMAKE_INSTALL_COMPONENT}" STREQUAL "Unspecified" OR NOT CMAKE_INSTALL_COMPONENT)
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/include/nupack/../extlib/tclap" TYPE FILE FILES "/Users/sven/Desktop/IGEM/Program/progress/nupack/nupack3.2.1/extlib/tclap/CmdLine.h")
+endif()
+
+if("${CMAKE_INSTALL_COMPONENT}" STREQUAL "Unspecified" OR NOT CMAKE_INSTALL_COMPONENT)
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/include/nupack/../extlib/tclap" TYPE FILE FILES "/Users/sven/Desktop/IGEM/Program/progress/nupack/nupack3.2.1/extlib/tclap/CmdLineInterface.h")
+endif()
+
+if("${CMAKE_INSTALL_COMPONENT}" STREQUAL "Unspecified" OR NOT CMAKE_INSTALL_COMPONENT)
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/include/nupack/../extlib/tclap" TYPE FILE FILES "/Users/sven/Desktop/IGEM/Program/progress/nupack/nupack3.2.1/extlib/tclap/CmdLineOutput.h")
+endif()
+
+if("${CMAKE_INSTALL_COMPONENT}" STREQUAL "Unspecified" OR NOT CMAKE_INSTALL_COMPONENT)
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/include/nupack/../extlib/tclap" TYPE FILE FILES "/Users/sven/Desktop/IGEM/Program/progress/nupack/nupack3.2.1/extlib/tclap/Constraint.h")
+endif()
+
+if("${CMAKE_INSTALL_COMPONENT}" STREQUAL "Unspecified" OR NOT CMAKE_INSTALL_COMPONENT)
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/include/nupack/../extlib/tclap" TYPE FILE FILES "/Users/sven/Desktop/IGEM/Program/progress/nupack/nupack3.2.1/extlib/tclap/DocBookOutput.h")
+endif()
+
+if("${CMAKE_INSTALL_COMPONENT}" STREQUAL "Unspecified" OR NOT CMAKE_INSTALL_COMPONENT)
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/include/nupack/../extlib/tclap" TYPE FILE FILES "/Users/sven/Desktop/IGEM/Program/progress/nupack/nupack3.2.1/extlib/tclap/HelpVisitor.h")
+endif()
+
+if("${CMAKE_INSTALL_COMPONENT}" STREQUAL "Unspecified" OR NOT CMAKE_INSTALL_COMPONENT)
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/include/nupack/../extlib/tclap" TYPE FILE FILES "/Users/sven/Desktop/IGEM/Program/progress/nupack/nupack3.2.1/extlib/tclap/IgnoreRestVisitor.h")
+endif()
+
+if("${CMAKE_INSTALL_COMPONENT}" STREQUAL "Unspecified" OR NOT CMAKE_INSTALL_COMPONENT)
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/include/nupack/../extlib/tclap" TYPE FILE FILES "/Users/sven/Desktop/IGEM/Program/progress/nupack/nupack3.2.1/extlib/tclap/MultiArg.h")
+endif()
+
+if("${CMAKE_INSTALL_COMPONENT}" STREQUAL "Unspecified" OR NOT CMAKE_INSTALL_COMPONENT)
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/include/nupack/../extlib/tclap" TYPE FILE FILES "/Users/sven/Desktop/IGEM/Program/progress/nupack/nupack3.2.1/extlib/tclap/MultiSwitchArg.h")
+endif()
+
+if("${CMAKE_INSTALL_COMPONENT}" STREQUAL "Unspecified" OR NOT CMAKE_INSTALL_COMPONENT)
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/include/nupack/../extlib/tclap" TYPE FILE FILES "/Users/sven/Desktop/IGEM/Program/progress/nupack/nupack3.2.1/extlib/tclap/OptionalUnlabeledTracker.h")
+endif()
+
+if("${CMAKE_INSTALL_COMPONENT}" STREQUAL "Unspecified" OR NOT CMAKE_INSTALL_COMPONENT)
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/include/nupack/../extlib/tclap" TYPE FILE FILES "/Users/sven/Desktop/IGEM/Program/progress/nupack/nupack3.2.1/extlib/tclap/StandardTraits.h")
+endif()
+
+if("${CMAKE_INSTALL_COMPONENT}" STREQUAL "Unspecified" OR NOT CMAKE_INSTALL_COMPONENT)
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/include/nupack/../extlib/tclap" TYPE FILE FILES "/Users/sven/Desktop/IGEM/Program/progress/nupack/nupack3.2.1/extlib/tclap/StdOutput.h")
+endif()
+
+if("${CMAKE_INSTALL_COMPONENT}" STREQUAL "Unspecified" OR NOT CMAKE_INSTALL_COMPONENT)
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/include/nupack/../extlib/tclap" TYPE FILE FILES "/Users/sven/Desktop/IGEM/Program/progress/nupack/nupack3.2.1/extlib/tclap/SwitchArg.h")
+endif()
+
+if("${CMAKE_INSTALL_COMPONENT}" STREQUAL "Unspecified" OR NOT CMAKE_INSTALL_COMPONENT)
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/include/nupack/../extlib/tclap" TYPE FILE FILES "/Users/sven/Desktop/IGEM/Program/progress/nupack/nupack3.2.1/extlib/tclap/UnlabeledMultiArg.h")
+endif()
+
+if("${CMAKE_INSTALL_COMPONENT}" STREQUAL "Unspecified" OR NOT CMAKE_INSTALL_COMPONENT)
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/include/nupack/../extlib/tclap" TYPE FILE FILES "/Users/sven/Desktop/IGEM/Program/progress/nupack/nupack3.2.1/extlib/tclap/UnlabeledValueArg.h")
+endif()
+
+if("${CMAKE_INSTALL_COMPONENT}" STREQUAL "Unspecified" OR NOT CMAKE_INSTALL_COMPONENT)
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/include/nupack/../extlib/tclap" TYPE FILE FILES "/Users/sven/Desktop/IGEM/Program/progress/nupack/nupack3.2.1/extlib/tclap/ValueArg.h")
+endif()
+
+if("${CMAKE_INSTALL_COMPONENT}" STREQUAL "Unspecified" OR NOT CMAKE_INSTALL_COMPONENT)
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/include/nupack/../extlib/tclap" TYPE FILE FILES "/Users/sven/Desktop/IGEM/Program/progress/nupack/nupack3.2.1/extlib/tclap/ValuesConstraint.h")
+endif()
+
+if("${CMAKE_INSTALL_COMPONENT}" STREQUAL "Unspecified" OR NOT CMAKE_INSTALL_COMPONENT)
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/include/nupack/../extlib/tclap" TYPE FILE FILES "/Users/sven/Desktop/IGEM/Program/progress/nupack/nupack3.2.1/extlib/tclap/VersionVisitor.h")
+endif()
+
+if("${CMAKE_INSTALL_COMPONENT}" STREQUAL "Unspecified" OR NOT CMAKE_INSTALL_COMPONENT)
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/include/nupack/../extlib/tclap" TYPE FILE FILES "/Users/sven/Desktop/IGEM/Program/progress/nupack/nupack3.2.1/extlib/tclap/Visitor.h")
+endif()
+
+if("${CMAKE_INSTALL_COMPONENT}" STREQUAL "Unspecified" OR NOT CMAKE_INSTALL_COMPONENT)
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/include/nupack/../extlib/tclap" TYPE FILE FILES "/Users/sven/Desktop/IGEM/Program/progress/nupack/nupack3.2.1/extlib/tclap/XorHandler.h")
+endif()
+
+if("${CMAKE_INSTALL_COMPONENT}" STREQUAL "Unspecified" OR NOT CMAKE_INSTALL_COMPONENT)
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/include/nupack/../extlib/tclap" TYPE FILE FILES "/Users/sven/Desktop/IGEM/Program/progress/nupack/nupack3.2.1/extlib/tclap/ZshCompletionOutput.h")
+endif()
+
+if("${CMAKE_INSTALL_COMPONENT}" STREQUAL "Unspecified" OR NOT CMAKE_INSTALL_COMPONENT)
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/include/nupack/../extlib/jsoncpp/json" TYPE FILE FILES "/Users/sven/Desktop/IGEM/Program/progress/nupack/nupack3.2.1/extlib/jsoncpp/json/json-forwards.h")
+endif()
+
+if("${CMAKE_INSTALL_COMPONENT}" STREQUAL "Unspecified" OR NOT CMAKE_INSTALL_COMPONENT)
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/include/nupack/../extlib/jsoncpp/json" TYPE FILE FILES "/Users/sven/Desktop/IGEM/Program/progress/nupack/nupack3.2.1/extlib/jsoncpp/json/json.h")
+endif()
+
